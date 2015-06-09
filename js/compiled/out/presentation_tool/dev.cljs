67a126e6bd38d2c5f29a674b002a1eadb492c092
Max Gonzih <gonzih@gmail.com>
2015-06-09 17:33:40 +0200

    Site updated on

--- js/compiled/out/presentation_tool/dev.cljs
+++
@@ -1,9 +0,0 @@
-(ns presentation-tool.dev
-    (:require
-     [presentation-tool.core]
-     [figwheel.client :as fw]))
-
-(fw/start {
-  :on-jsload (fn []
-               ;; (stop-and-start-my app)
-               )})
